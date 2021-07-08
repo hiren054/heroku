91dabf3f06c6ad61626c12531b3742797d8dd1bc
Hiren@321 <rathodhiren054@gmail.com>
2021-07-08 16:47:41 +0530

    try to deploy on heroku

--- app.py
+++ app.py
@@ -6,9 +6,15 @@ app = Flask(__name__)
 def home():
     return "<h1> Jay yogeshwar!! </h1>"
 
+@app.route('/home')
+def about():
+    return "<h1> Home page!! </h1>"
+
 @app.route('/about')
 def about():
-    return "<h1> About </h1>"
+    return "<h1> About!! </h1>"
+
+
 
 if __name__ == '__main__' :
     app.run(debug=True)
